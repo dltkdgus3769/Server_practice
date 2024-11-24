86e6173dfb31faa4273ad81a53d143479ccb7b9c
dltkdgus3769 <tkdgus3769@naver.com>
2024-11-24 18:56:07 +0900

    test 2

---
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/VoTest.java
@@ -0,0 +1,11 @@
+package com.busanit501.helloworld.jdbcex;
+
+import com.busanit501.helloworld.jdbcex.dto.TodoVO;
+import org.junit.jupiter.api.Test;
+
+public class VoTest {
+    @Test
+    public void test(){
+        TodoVO todoVO = new TodoVO();
+    }
+}
